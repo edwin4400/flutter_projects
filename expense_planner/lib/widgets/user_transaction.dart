3148e296fcc255acb9feb335d813cb13c382c7dc
Edwin <edwin4400@yahoo.com>
2021-06-01 08:07:06 +0800

    stateful vs stateless

---
+++ expense_planner/lib/widgets/user_transaction.dart
@@ -0,0 +1,36 @@
+import 'package:expense_planner/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'new_transaction.dart';
+import 'transaction_list.dart';
+
+class UserTransactions extends StatefulWidget {
+  @override
+  _UserTransactionsState createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 40.99,
+      date: DateTime.now(),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        NewTransaction(),
+        TransactionList(),
+      ],
+    );
+  }
+}
